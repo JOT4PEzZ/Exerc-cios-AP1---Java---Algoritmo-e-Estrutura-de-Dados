17d593047d7482fd10ce064cca6cce01dd04da0d
JOT4PEzZ <joaopedrovm05@gmail.com>
2025-09-09 00:30:37 -0300

    Update Calculadora.java

--- Calculadora.java
+++ Calculadora.java
@@ -3,23 +3,24 @@ package calculadora;
 public class Calculadora {
 
 	public Double soma(double d, double e) {
-		// TODO Auto-generated method stub
+		
 		return d + e;
 	}
 
 	public Double multiplica(double d, double e) {
-		// TODO Auto-generated method stub
+		
 		return d * e;
 	}
 
 	public Double divide(double d, double e) {
-		// TODO Auto-generated method stub
+		
 		return d/e;
 	}
 
 	public Double potencia(double d, double e) {
-		// TODO Auto-generated method stub
+		
 		return Math.pow(d,e);
 	}
 
 }
+
